f381547a98265ed9e22c3f65a7851c69f5617a03
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-08-27 14:46:36 -0600

    commit Rol

---
+++ Proyecto-React/src/assets/ReportesRoles.jsx
@@ -0,0 +1,16 @@
+
+export const ReporteRol = () => {
+    return (
+      <><h1>ReporteRol</h1><table>
+  
+            <th>Nombre</th>
+            <th>Fecha Desactivado</th>
+              <tbody>
+      <tr>
+          <td>Elvis Flores</td>
+          <td>Dato</td>
+      </tr>
+  </tbody>
+        </table></>
+    )
+  }
--- Proyecto-React/src/main.jsx
+++ Proyecto-React/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Horarios } from './assets/Horarios'
 import { ReporteHorarios } from './assets/ReporteHorarios'
 import { Rol } from './assets/Rol'
+import { ReporteRol } from './assets/ReportesRoles'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -20,5 +21,11 @@ ReactDOM.createRoot(document.getElementById('ReporteHorarios')).render(
 ReactDOM.createRoot(document.getElementById('Rol')).render(
   <React.StrictMode>
     <Rol />
+  </React.StrictMode>
+)
+
+ReactDOM.createRoot(document.getElementById('ReporteRol')).render(
+  <React.StrictMode>
+    <ReporteRol />
   </React.StrictMode>
 )
