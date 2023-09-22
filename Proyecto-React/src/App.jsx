5aec3c88dc965f30f06748203f29a97967265fbc
SRodriguez0801 <131110417+SRodriguez0801@users.noreply.github.com>
2023-09-21 22:46:39 -0600

    agregar login

--- Proyecto-React/src/App.jsx
+++ Proyecto-React/src/App.jsx
@@ -6,29 +6,48 @@ import { Rol } from './componentes/Rol'
 import { Usuario } from './componentes/Usuario'
 import { Menu } from './componentes/Menu'
 import { Routes, Route } from 'react-router-dom'
-
+import { useState, useCallback } from 'react'; 
+import { Link } from 'react-router-dom'; 
+import { Login } from './componentes/Login/Login'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-
-
+  const handleLogin = (isLoggedIn) => {
+    setIsAuthenticated(isLoggedIn);
+  };
  
   return (
     <>
-
-<Menu/>
-    <Routes>
-    <Route path='/' element={ <Inicio />}  ></Route>
-    <Route path='/horarios' element={ <Horarios />}  ></Route>
-    <Route path='/laboratorio' element={ <Laboratorio />}  ></Route>
-    <Route path='/reservas' element={ <Reservas />}  ></Route>
-    <Route path='/rol' element={ <Rol />}  ></Route>
-    <Route path='/usuario' element={ <Usuario />}  ></Route>
-    </Routes> 
-
-  
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/horarios' element={<Horarios />} />
+        <Route path='/laboratorio' element={<Laboratorio />} />
+        <Route path='/reservas' element={<Reservas />} />
+        <Route path='/rol' element={<Rol />} />
+        <Route path='/usuario' element={<Usuario />} />
+        {/* Add a new route for the Login component */}
+        <Route
+          path='/login'
+          element={
+            isAuthenticated ? (
+              // Redirect to another page if the user is already authenticated
+              <Navigate to='/' />
+            ) : (
+              // Render the Login component if the user is not authenticated
+              <Login dataSesion={handleLogin} />
+            )
+          }
+        />
+      </Routes>
     </>
   );
 }
 
-export default App
+export default App;
+
+
+
+
+
