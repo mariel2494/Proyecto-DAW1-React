59951138c70046491165a4e2378f8a60251f462f
mariel2494 <dinorammontoya@hotmail.com>
2023-09-22 18:08:34 -0600

    cambios

--- Proyecto-React/src/componentes/Laboratorio.jsx
+++ Proyecto-React/src/componentes/Laboratorio.jsx
@@ -1,48 +1,102 @@
-//Gustavo
+import  { React, useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const url = "http://localhost:3000/api/laboratorio";
+
 
 export const Laboratorio = () => {
+
+  
+  const [datos, setDatos] = useState([]);
+  const [nombre, setNombre] = useState('');
+  const [error, setError] = useState(null);
+
+  const getLaboratorio = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error al obtener datos del servidor");
+      }
+      const data = await response.json();
+      setDatos(data);
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getLaboratorio();
+  }, []);
+
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
+  };
+
+
+
+
+
+
+  const postLaboratorio = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("nombre", nombre);
+
+
+
+      const response = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al enviar datos al servidor");
+      }
+
+      // Actualizar la lista de datos después de enviar el nuevo producto
+      getLaboratorio();
+
+      // Limpiar los campos después de enviar
+      setNombre("");
+
+  
+    } catch (error) {
+      console.error(error);
+    
+    }
+  };
+
   return (
     <>
-      <div className="container">
-        <form>
-          <h1 className="text-center">Crear Laboratorios</h1>
+      <div className="container" style={{ maxWidth: "500px", margin: "0 auto", padding: "20px" }}>
+        <h1 className="text-center">Crear Laboratorios</h1>
 
-          <div className="form-group mb-3">
-            <label className="form-label">Nombre</label>
-            <input type="text" className="form-control" id="nombre" name="nombre" placeholder="Nombre" />
-          </div>
-
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="activo" checked />
-            <label className="form-check-label" >Activo</label>
-          </div>
-
+        <form onSubmit={postLaboratorio}>
           <div className="mb-3">
-            <label className="form-label">Fecha Desactivado</label>
-            <input type="date" className="form-control" id="fecha" name="fecha" disabled />
+            <label className="form-label">Nombre</label>
+            <input
+              type="text"
+              className="form-control"
+              value={nombre}
+              onChange={handleNombreChange}
+            />
           </div>
 
-          <button type="submit" className="btn btn-primary">Guardar</button>
+
+
+
+          <button type="submit" className="btn btn-primary">
+            Crear Laboratorio
+          </button>
+          <br /><br />
         </form>
 
-        <table className="table mt-4">
-          <thead>
-            <tr>
-              <th scope="col">Nombre</th>
-              <th scope="col">Activo</th>
-              <th scope="col">Fecha Desactivado</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td scope="row">Nombre Ejemplo</td>
-              <td>✔️</td>
-              <td>2023-08-27</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
 
+
+      </div>
     </>
   )
 }
